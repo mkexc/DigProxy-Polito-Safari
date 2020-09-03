8b3f9ea22422d4c0c68b950ec4a962ab1f11c1dd
Niccolò Marcucci <niccolo.marcucci@polito.it>
2020-09-03 18:27:00 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+    var url = activeTab.url;
+    var start_pos = url.indexOf("//")+2;
+    var protocol = url.slice(0,start_pos-1);
+    var end_pos = url.indexOf("/",start_pos);
+    var remaining = url.slice(end_pos,-1);
+    var mainUrl = url.slice(start_pos,end_pos);
+    var newURL = protocol + mainUrl.replace(/\./g,"-") +  ".ezproxy.biblio.polito.it"+ remaining;
+    chrome.tabs.create({url: newURL});
+});
