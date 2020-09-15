5701dd657d4105269afa1ab545abccc352b02cc8
Niccolò Marcucci <niccolo.marcucci@polito.it>
2020-09-15 15:35:35 +0200

    follow mozilla suggestion for browser chenge description

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-chrome.browserAction.onClicked.addListener(function (activeTab) {
+browser.browserAction.onClicked.addListener(function (activeTab) {
     var url = activeTab.url;
     var start_pos = url.indexOf("//")+2;
     var protocol = url.slice(0,start_pos-1);
     var end_pos = url.indexOf("/",start_pos);
-    var remaining = url.slice(end_pos,-1);
+    var remaining = url.slice(end_pos);
     var mainUrl = url.slice(start_pos,end_pos);
     var newURL = protocol + mainUrl.replace(/\./g,"-") +  ".ezproxy.biblio.polito.it"+ remaining;
-    chrome.tabs.create({url: newURL});
+    browser.tabs.create({url: newURL});
 });
